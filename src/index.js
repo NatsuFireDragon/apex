58600ff0e80558b9a51e9bd5f3afbea90b98e8f2
NastuFireDragon <[henry.sean.r@gmail.com]>
2021-01-15 09:37:47 -0600

    removed styles.scss, added index.scss file. Added completed menu, hover styling, as well as added a button to the test results

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-// import "/src/scss/styles.scss";
+// Mobile Menu
+const menuBars = document.querySelector(".menu-bars");
+const mobileList = document
+  .querySelector(".mobile-list")
+  .querySelectorAll("li");
+const mobileNav = document.querySelector(".mobile-menu");
+
+menuBars.addEventListener("click", () => {
+  mobileNav.style.width = "100%";
+});
+
+for (let i = 0; i < mobileList.length; i++) {
+  mobileList[i].addEventListener("click", () => {
+    mobileNav.style.width = "0%";
+  });
+}
+
 
